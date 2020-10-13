ef540b28e3577acf356c7c73530135e3e564a39f
DhanushM12 <dhanushmukhi@gmail.com>
2020-10-14 00:23:15 +0530

    added the actions required

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+export const SHAPE_TYPES = {
+  RECT: "rect",
+};
+export const DEFAULTS = {
+  RECT: {
+    STROKE: "#000000",
+    FILL: "#ffffff",
+    WIDTH: 150,
+    HEIGHT: 100,
+    ROTATION: 0,
+  },
+};
+
+export const LIMITS = {
+  RECT: {
+    MAX: 1000,
+    MIN: 10,
+  },
+};
+
+export const DRAG_DATA_KEY = "__drag_data_payload__";
