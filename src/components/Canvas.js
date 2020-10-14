20568eb76f5d2a4ad8618942af17f7304938b576
DhanushM12 <dhanushmukhi@gmail.com>
2020-10-14 06:14:53 +0530

    added react-konva for canvas graphics

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Layer, Stage } from "react-konva";
 
 function Canvas() {
-  return <div className="canvas">Canvas Rectangle</div>;
+  return (
+    <main className="canvas">
+      <Stage width={window.innerWidth - 400} height={window.innerHeight}>
+        <Layer />
+      </Stage>
+    </main>
+  );
 }
 
 export default Canvas;
